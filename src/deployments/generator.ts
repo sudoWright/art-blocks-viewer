8b78ec6b2ce3f9fc86a9a5467e5cf481f676f7ff
ryley-o <30364988+ryley-o@users.noreply.github.com>
2024-10-31 16:08:17 -0700

    Update generator.ts with mainnet deployment

--- src/deployments/generator.ts
+++ src/deployments/generator.ts
@@ -1,6 +1,6 @@
 export const generatorDeployments: {
   [key: string]: `0x${string}`;
 } = {
-  "1": "0xtodo",
+  "1": "0x953D288708bB771F969FCfD9BA0819eF506Ac718",
   "11155111": "0xdC862938cA0a2D8dcabe5733C23e54ac7aAFFF27",
 };
