178fec84187ee5ae991822dfe74a39cdd0a6be9a
Ben Yoshiwara <yoshiwara.b@gmail.com>
2024-11-19 17:06:59 -0800

    remove gh-pages stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "./404.html",
-          dest: "./",
-        },
-      ],
-    }),
-  ],
-  base: "/on-chain-generator-viewer",
+  plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
